eeeb232f071aab290f8b26edc12f1f06de03a419
shibamoto <silverbirder@gmail.com>
2023-04-04 23:32:56 +0900

    :white_check_mark: test: add mutate test

---
+++ stryker.conf.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+/** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+const config = {
+  _comment:
+    "This config was generated using 'stryker init'. Please see the guide for more information: https://stryker-mutator.io/docs/stryker-js/guides/react",
+  testRunner: "jest",
+  reporters: ["progress", "clear-text", "html"],
+  coverageAnalysis: "off",
+  jest: {
+    projectType: "create-react-app",
+  },
+  checkers: ["typescript"],
+  tsconfigFile: "tsconfig.json",
+  typescriptChecker: {
+    prioritizePerformanceOverAccuracy: true,
+  },
+  mutate: ["src/**/!(*.{stories,test}).{ts,tsx}"],
+};
+export default config;
