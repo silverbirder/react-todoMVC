bc6cc7b18800098a7ad7560ec0053efc7a116852
shibamoto <silverbirder@gmail.com>
2023-04-20 22:31:30 +0900

    test: add TodoItem.test.tsx

--- src/components/TodoItem/TodoItem.test.tsx
+++ src/components/TodoItem/TodoItem.test.tsx
@@ -1,3 +1,59 @@
-import {} from ".";
+import React from "react";
+import {render, screen} from "@testing-library/react";
+import {TodoItem} from "./TodoItem";
+import {TodoContext} from "../context";
+import userEvent from "@testing-library/user-event";
 
-test.todo("TodoItem");
+const mockToggle = jest.fn();
+const mockDeleteTodo = jest.fn();
+const mockAdd = jest.fn();
+
+const mockTodo = {
+    id: "1",
+    title: "Test Todo",
+    completed: false,
+};
+
+describe("TodoItem", () => {
+    it("renders todo title", () => {
+        // Act
+        render(
+            <TodoContext.Provider value={{toggle: mockToggle, deleteTodo: mockDeleteTodo, addTodo: mockAdd}}>
+                <TodoItem todo={mockTodo}/>
+            </TodoContext.Provider>
+        );
+
+        // Assert
+        expect(screen.getByText(mockTodo.title)).toBeInTheDocument();
+    });
+
+    it("toggles todo completion", () => {
+        // Arrange
+        render(
+            <TodoContext.Provider value={{toggle: mockToggle, deleteTodo: mockDeleteTodo, addTodo: mockAdd}}>
+                <TodoItem todo={mockTodo}/>
+            </TodoContext.Provider>
+        );
+
+        // Act
+        userEvent.click(screen.getByRole("checkbox"));
+
+        // Assert
+        expect(mockToggle).toHaveBeenCalledWith(mockTodo.id);
+    });
+
+    it("deletes a todo", () => {
+        // Arrange
+        render(
+            <TodoContext.Provider value={{toggle: mockToggle, deleteTodo: mockDeleteTodo, addTodo: mockAdd}}>
+                <TodoItem todo={mockTodo}/>
+            </TodoContext.Provider>
+        );
+
+        // Act
+        userEvent.click(screen.getByRole("button"));
+
+        // Assert
+        expect(mockDeleteTodo).toHaveBeenCalledWith(mockTodo.id);
+    });
+});
